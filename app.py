215d3d8918555413f18c623c7f97016c63cbe3a5
Harrison Chase <hw.chase.17@gmail.com>
2023-02-07 08:20:03 -0800

    cr

--- app.py
+++ app.py
@@ -1,17 +1,102 @@
+import os
+from typing import Optional, Tuple
+
+import gradio as gr
 import pickle
 from query_data import get_chain
+from threading import Lock
+
+with open("vectorstore.pkl", "rb") as f:
+    vectorstore = pickle.load(f)
 
 
-if __name__ == "__main__":
-    with open("vectorstore.pkl", "rb") as f:
-        vectorstore = pickle.load(f)
-    qa_chain = get_chain(vectorstore)
-    chat_history = []
-    print("Chat with your docs!")
-    while True:
-        print("Human:")
-        question = input()
-        result = qa_chain({"question": question, "chat_history": chat_history})
-        chat_history.append((question, result["answer"]))
-        print("AI:")
-        print(result["answer"])
+def set_openai_api_key(api_key: str):
+    """Set the api key and return chain.
+    If no api_key, then None is returned.
+    """
+    if api_key:
+        os.environ["OPENAI_API_KEY"] = api_key
+        chain = get_chain(vectorstore)
+        os.environ["OPENAI_API_KEY"] = ""
+        return chain
+
+class ChatWrapper:
+
+    def __init__(self):
+        self.lock = Lock()
+    def __call__(
+        self, api_key: str, inp: str, history: Optional[Tuple[str, str]], chain
+    ):
+        """Execute the chat functionality."""
+        self.lock.acquire()
+        try:
+            history = history or []
+            # If chain is None, that is because no API key was provided.
+            if chain is None:
+                history.append((inp, "Please paste your OpenAI key to use"))
+                return history, history
+            # Set OpenAI key
+            import openai
+            openai.api_key = api_key
+            # Run chain and append input.
+            output = chain({"question": inp, "chat_history": history})["answer"]
+            history.append((inp, output))
+        except Exception as e:
+            raise e
+        finally:
+            self.lock.release()
+        return history, history
+
+chat = ChatWrapper()
+
+block = gr.Blocks(css=".gradio-container {background-color: lightgray}")
+
+with block:
+    with gr.Row():
+        gr.Markdown("<h3><center>LangChain Demo</center></h3>")
+
+        openai_api_key_textbox = gr.Textbox(
+            placeholder="Paste your OpenAI API key (sk-...)",
+            show_label=False,
+            lines=1,
+            type="password",
+        )
+
+    chatbot = gr.Chatbot()
+
+    with gr.Row():
+        message = gr.Textbox(
+            label="What's your question?",
+            placeholder="Ask questions about the most recent state of the union",
+            lines=1,
+        )
+        submit = gr.Button(value="Send", variant="secondary").style(full_width=False)
+
+    gr.Examples(
+        examples=[
+            "What did the president say about Kentaji Brown Jackson",
+            "Did he mention Stephen Breyer?",
+            "What was his stance on Ukraine",
+        ],
+        inputs=message,
+    )
+
+    gr.HTML("Demo application of a LangChain chain.")
+
+    gr.HTML(
+        "<center>Powered by <a href='https://github.com/hwchase17/langchain'>LangChain 🦜️🔗</a></center>"
+    )
+
+    state = gr.State()
+    agent_state = gr.State()
+
+    submit.click(chat, inputs=[openai_api_key_textbox, message, state, agent_state], outputs=[chatbot, state])
+    message.submit(chat, inputs=[openai_api_key_textbox, message, state, agent_state], outputs=[chatbot, state])
+
+    openai_api_key_textbox.change(
+        set_openai_api_key,
+        inputs=[openai_api_key_textbox],
+        outputs=[agent_state],
+    )
+
+block.launch(debug=True)
---
+++ cli_app.py
@@ -0,0 +1,17 @@
+import pickle
+from query_data import get_chain
+
+
+if __name__ == "__main__":
+    with open("vectorstore.pkl", "rb") as f:
+        vectorstore = pickle.load(f)
+    qa_chain = get_chain(vectorstore)
+    chat_history = []
+    print("Chat with your docs!")
+    while True:
+        print("Human:")
+        question = input()
+        result = qa_chain({"question": question, "chat_history": chat_history})
+        chat_history.append((question, result["answer"]))
+        print("AI:")
+        print(result["answer"])
